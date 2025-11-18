368692426ac200a3919d72f41ad355e669057ea1
Sandeep Battula <sandeep.battula@nagarro.com>
2025-11-18 19:59:15 +0530

    Fix run issues
    Signed-off-by: Sandeep Battula <sandeep.battula@nagarro.com>

--- code/boot/src/main/java/com/ai/app/config/CasClientConfig.java
+++ code/boot/src/main/java/com/ai/app/config/CasClientConfig.java
@@ -5,36 +5,34 @@ import com.ai.app.cas.gen.api.UsersApi;
 import com.ai.app.cas.gen.invoker.ApiClient;
 import com.ai.app.common.props.ClientProps;
 import com.ai.app.common.props.RestBuilderProps;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties(ClientProps.class)
 public class CasClientConfig {
 
-  @Bean
-  public ApiClient apiClient(ClientProps clientProps) {
-    RestBuilderProps restBuilderProps = clientProps.get("cas-client");
+    @Bean
+    public ApiClient apiClient(ClientProps clientProps) {
+        RestBuilderProps restBuilderProps = clientProps.get("cas-client");
 
-    RestClient client =
-        RestClient.builder()
-            .baseUrl(restBuilderProps.getBaseUrl())
-            .defaultHeader("Authorization", "Bearer " + restBuilderProps.getToken())
-            .defaultHeader("Content-Type", "application/json")
-            .defaultHeader("Accept", "application/json")
-            .build();
-    return new ApiClient(client);
-  }
+        RestClient client =
+                RestClient.builder()
+                        .baseUrl(restBuilderProps.getBaseUrl())
+                        .defaultHeader("Authorization", "Bearer " + restBuilderProps.getToken())
+                        .defaultHeader("Content-Type", "application/json")
+                        .defaultHeader("Accept", "application/json")
+                        .build();
+        return new ApiClient(client);
+    }
 
-  @Bean
-  public AuthenticationApi authenticationApi(ApiClient apiClient) {
-    return new AuthenticationApi(apiClient);
-  }
+    @Bean
+    public AuthenticationApi authenticationApi(ApiClient apiClient) {
+        return new AuthenticationApi(apiClient);
+    }
 
-  @Bean
-  public UsersApi usersApi(ApiClient apiClient) {
-    return new UsersApi(apiClient);
-  }
+    @Bean
+    public UsersApi usersApi(ApiClient apiClient) {
+        return new UsersApi(apiClient);
+    }
 }
--- code/common/src/main/java/com/ai/app/common/props/ClientProps.java
+++ code/common/src/main/java/com/ai/app/common/props/ClientProps.java
@@ -1,13 +1,12 @@
 package com.ai.app.common.props;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ConfigurationProperties(prefix = "app.dominance")
-@Component
 @Data
 public class ClientProps {
 
