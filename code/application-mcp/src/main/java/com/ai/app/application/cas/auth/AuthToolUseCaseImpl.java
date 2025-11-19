7c01adcab15f5e80717a4fa2eb7f04d662ea98d4
Sandeep Battula <sandeep.battula@nagarro.com>
2025-11-19 19:05:55 +0530

    some more content changes

--- code/application-mcp/src/main/java/com/ai/app/application/cas/auth/AuthToolUseCaseImpl.java
+++ code/application-mcp/src/main/java/com/ai/app/application/cas/auth/AuthToolUseCaseImpl.java
@@ -7,7 +7,9 @@ import com.ai.app.usecase.cas.auth.AuthToolUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springaicommunity.mcp.annotation.McpTool;
 import org.springaicommunity.mcp.annotation.McpToolParam;
+import org.springaicommunity.mcp.context.McpSyncRequestContext;
 import org.springframework.stereotype.Component;
+import org.stringtemplate.v4.ST;
 
 @Component
 @RequiredArgsConstructor
@@ -21,8 +23,16 @@ public class AuthToolUseCaseImpl implements AuthToolUseCase {
           "Authenticate a user with username and password and fetch back the user details.")
   @Override
   public TokenDTO authenticateUser(
+      McpSyncRequestContext context,
       @McpToolParam(description = "Username of the user") String username,
       @McpToolParam(description = "Password of the user") String password) {
+
+    ST st = new ST("Trying to search for the user details with user name: <username>");
+    st.add("username", username);
+    context.info(st.render());
+
+    context.progress(p -> p.progress(50).total(100).message("Authenticating user"));
+
     return this.authToolService.authenticateUser(username, password);
   }
 
@@ -30,7 +40,7 @@ public class AuthToolUseCaseImpl implements AuthToolUseCase {
       name = "get_current_user_info",
       description = "Get the current logged in user details using the token")
   @Override
-  public UserFullDetailsDTO getCurrentUserInfo() {
+  public UserFullDetailsDTO getCurrentUserInfo(McpSyncRequestContext context) {
     return this.authToolService.getCurrentUserInfo();
   }
 
--- code/domain/src/main/java/com/ai/app/usecase/cas/auth/AuthToolUseCase.java
+++ code/domain/src/main/java/com/ai/app/usecase/cas/auth/AuthToolUseCase.java
@@ -2,12 +2,13 @@ package com.ai.app.usecase.cas.auth;
 
 import com.ai.app.model.cas.auth.TokenDTO;
 import com.ai.app.model.cas.auth.UserFullDetailsDTO;
+import org.springaicommunity.mcp.context.McpSyncRequestContext;
 
 public interface AuthToolUseCase {
 
-  TokenDTO authenticateUser(String username, String password);
+  TokenDTO authenticateUser(McpSyncRequestContext context, String username, String password);
 
-  UserFullDetailsDTO getCurrentUserInfo();
+  UserFullDetailsDTO getCurrentUserInfo(McpSyncRequestContext context);
 
   TokenDTO refreshToken();
 }
